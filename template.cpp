c740b32fa8057cd7b46d0b6f5938f006fad65ca6
Prakhar Srivastava <prakharrrr4@gmail.com>
2021-09-13 16:29:42 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,52 @@
+/*
+ _One_last_try
+*/
+
+
+
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector")
+#include <bits/stdc++.h>
+#include<set>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define pb push_back
+#define ff first
+#define ss second
+
+
+
+
+#define si(x) int(x.size())
+#define sum_all(a)     ( accumulate ((a).begin(), (a).end(), 0ll))
+#define mine(a)    (*min_element((a).begin(), (a).end()))
+#define maxe(a)    (*max_element((a).begin(), (a).end()))
+#define mini(a)    ( min_element((a).begin(), (a).end()) - (a).begin())
+#define maxi(a)    ( max_element((a).begin(), (a).end()) - (a).begin())
+
+
+
+void solve()
+{
+ 
+  
+   
+}
+
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+
+
+    //int t; cin >> t; while (t--)
+    solve();
+
+}
+
